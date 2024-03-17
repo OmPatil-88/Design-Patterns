253fd93d1bbe4a0f90d84e8226a733f682f37ac8
om patil <patilom139@gmail.com>
2024-03-17 17:20:34 +0530

    Added ReleaseInstace()

--- Singleton/SingleTonThreadSafe.cpp
+++ Singleton/SingleTonThreadSafe.cpp
@@ -5,17 +5,28 @@
 #include <mutex>
 #include <chrono>
 
+typedef int t_instance_release_status;
+typedef int t_instance_cout;
+#define STAUTS_REFCOUNT_REMOVED 1
+#define STAUTS_INSTANCE_RELEASED 2
+#define STAUTS_INVALID_INSTANCE 3
+
 class Singleton
 {
 
 private:
     static Singleton *singleton;
     static std::mutex mutex_;
+    static t_instance_cout refCount;
 
     // Declare Variable as per requirement
     std::string str;
 
     Singleton(const std::string str) : str(str)
+    {
+    }
+
+    ~Singleton()
     {
     }
 
@@ -25,8 +36,9 @@ public:
     // Singleton should not be assignable
     void operator=(const Singleton &) = delete;
 
-    // make get instace cusotmize as per need
+    // make get instace customize as per need
     static Singleton *GetInstance(const std::string &value);
+    static t_instance_release_status ReleaseInstance();
     std::string value() const
     {
         return str;
@@ -37,6 +49,7 @@ public:
 };
 
 Singleton *Singleton::singleton = nullptr;
+int Singleton::refCount = 0;
 std::mutex Singleton::mutex_;
 
 Singleton *Singleton::GetInstance(const std::string &value)
@@ -45,9 +58,24 @@ Singleton *Singleton::GetInstance(const std::string &value)
     if (Singleton::singleton == nullptr)
     {
         Singleton::singleton = new Singleton(value);
+        refCount = 1;
     }
-
+    refCount;
     return singleton;
+}
+
+t_instance_release_status Singleton::ReleaseInstance()
+{
+    if (Singleton::singleton == nullptr)
+        return STAUTS_INVALID_INSTANCE;
+
+    std::lock_guard<std::mutex> lock(mutex_);
+    refCount--;
+    if (Singleton::refCount == 0)
+    {
+        return STAUTS_INSTANCE_RELEASED;
+    }
+    return STAUTS_REFCOUNT_REMOVED;
 }
 
 void Thread1()
@@ -55,6 +83,7 @@ void Thread1()
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     Singleton *singleton = Singleton::GetInstance("First Thread");
     std::cout << singleton->value() << std::endl;
+    Singleton::ReleaseInstance() == STAUTS_INSTANCE_RELEASED ? std::cout << "Instance Released\n" : std::cout << "RefCount Removed\n";
 }
 
 void Thread2()
@@ -62,6 +91,7 @@ void Thread2()
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     Singleton *singleton = Singleton::GetInstance("Second Thread");
     std::cout << singleton->value() << std::endl;
+    Singleton::ReleaseInstance() == STAUTS_INSTANCE_RELEASED ? std::cout << "Instace Released\n" : std::cout << "RefCount Removed\n";
 }
 
 int main()
